5f203bfb40ee18c5bac80ffa7e68f109ce64b306
myogaads <m.yogaadi1234@gmail.com>
2024-12-05 09:30:27 +0700

    Testing Production_1 Backend SMS v.2

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'd965c8bcd08e036eac65cd5e35f346aa8e530159',
+        'reference' => '395ec8a443a958ec60d6f990738685ad3d9e7499',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -424,7 +424,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'd965c8bcd08e036eac65cd5e35f346aa8e530159',
+            'reference' => '395ec8a443a958ec60d6f990738685ad3d9e7499',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -808,8 +808,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '3.0.0',
-                1 => '1.0|2.0|3.0',
+                0 => '1.0|2.0|3.0',
+                1 => '3.0.0',
             ),
         ),
         'psr/simple-cache' => array(
